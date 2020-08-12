f7d4c196ca1b74be8c344075a6393642f690efee
jgskmk <hnymtyaya@yahoo.co.jp>
2020-08-12 15:53:28 +0900

    add_relativetime tableheart

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -3,11 +3,11 @@ class LikeController < ApplicationController
     def create
      @like = Like.new(user_id: @current_user.id, post_id: params[:id])
       @like.save
-      redirect_to("/post/#{params[:id]}")
+      redirect_back(fallback_location:"/home/top/1")
     end
   def destroy
     @like = Like.find_by(user_id: @current_user.id, post_id: params[:id])
     @like.destroy
-    redirect_to("/post/#{params[:id]}")
+    redirect_back(fallback_location:"/home/top/1")
   end
 end
