720c4f12e2439e5a2e9e21be8d82b7ca556babe8
jgskmk <hnymtyaya@yahoo.co.jp>
2020-08-14 16:12:55 +0900

    add has_many

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,5 @@
 class PostController < ApplicationController
-  before_action :authenticate_user,only: [:create,:update]
+  before_action :authenticate_user,only: [:create,:update,:destroy]
   def show
     @post=Post.find_by(id: params[:id])
   end
@@ -18,17 +18,32 @@ class PostController < ApplicationController
   end
   def update
     @post=Post.find_by(id: params[:id])
-    if @current_user.id != @post.user_id.to_i
+    if @current_user.id != @post.user_id
       flash[:notice]="権限がありません。"
-      redirect_to("/home/top")
+      redirect_to("/home/top/1")
     end
   end
   def update_post
       @post.content=params[:content]
       @post.save
       flash[:notice]="投稿を編集しました。"
-      redirect_to("/home/top")
-
-
+      redirect_to("/home/top/1")
   end
+
+  def destroy
+    @post=Post.find_by(id: params[:id])
+    if @current_user.id != @post.user_id
+      flash[:notice]="権限がありません。"
+      redirect_to("/home/top/1")
+    else
+      @post.destroy
+      flash[:notice]="投稿を削除しました。"
+      redirect_to("/home/top/1")
+    end
+  end
+
+
+
+
+
 end
